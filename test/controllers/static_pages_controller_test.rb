2f268d68ce1ec5614b355eab88f59aa8cd239cc3
Charlie Owen <charlie@sonniesedge.co.uk>
2015-09-04 12:13:06 +0200

    Add static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Accessible screencaps"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Accessible screencaps"
+  end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+    assert_select "title", "Help | Accessible screencaps"
   end
 
 end
